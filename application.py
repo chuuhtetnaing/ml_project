9da7802a2824b878e1e6454d26a4a4eb97fa3796
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2019-12-02 15:22:28 +0800

    configure with basic machine learning model

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, session, render_template, redirect, url_for, request
 from bs4 import BeautifulSoup
 from joblib import load
 from joblib import dump
+import pandas as pd
 
 import requests
 from flask import jsonify
@@ -77,34 +78,82 @@ def improve():
 	information_list = list()
 	# ['Updated', 'Size', 'Installs', 'Current Version', 'Requires Android', 'Content Rating', 'In-app Products']
 
-	i = 1
-	for additional_information in additional_informations:
-		if i == 6:
-			check_content = additional_information.find("div", "BgcNfc").text
-			if check_content != "Content Rating":
-				information_list.append("No content rating is available")
+
+
+	if additional_informations[0].find("span", "htlgb").text == 'Learn More':
+		information_list.append(additional_informations[1].find("span", "htlgb").text)
+		information_list.append(additional_informations[2].find("span", "htlgb").text)
+		information_list.append(additional_informations[3].find("span", "htlgb").text)
+		information_list.append(additional_informations[4].find("span", "htlgb").text)
+		information_list.append(additional_informations[5].find("span", "htlgb").text)
+		additional_informations = soup.find_all("div", "hAyfc")
+		additional_informations = list(map(additional_informations.__getitem__, [6]))
+		information_list.append(additional_informations[0].find("span", "htlgb").text)
+		additional_informations = soup.find_all("div", "hAyfc")
+		additional_informations = list(map(additional_informations.__getitem__, [8]))
+		information_list.append((additional_informations[0].find("span", "htlgb").text).replace(u'\xa0', u' '))
+	else:
+		i = 1
+		for additional_information in additional_informations:
+			if i == 6:
+				check_content = additional_information.find("div", "BgcNfc").text
+				if check_content != "Content Rating":
+					information_list.append("No content rating is available")
+				else:
+					content_rating = additional_information.find_all("div")[2].text
+					information_list.append(content_rating)
+			elif i == 7:
+				check_content = additional_information.find("div", "BgcNfc").text
+				if check_content != "In-app Products":
+					information_list.append("No in-app purchase is shown")
+				else:
+					in_app_purchase = additional_information.find("span", "htlgb").text
+					information_list.append(in_app_purchase)
 			else:
-				content_rating = additional_information.find_all("div")[2].text
-				information_list.append(content_rating)
-		elif i == 7:
-			check_content = additional_information.find("div", "BgcNfc").text
-			if check_content != "In-app Products":
-				information_list.append("No in-app purchase is shown")
-			else:
-				in_app_purchase = additional_information.find("span", "htlgb").text
-				information_list.append(in_app_purchase)
-		else:
-			other_information = additional_information.find("span", "htlgb").text
-			information_list.append(other_information)
-		i = i + 1
+				other_information = additional_information.find("span", "htlgb").text
+				information_list.append(other_information)
+			i = i + 1
 
 	category = soup.find_all("a", "hrTbp R8zArc")[1]['href']
 	category = category.split("/")[-1]
 	information_list.append(category)
 
+	count = "".join(count.split(','))
+	dataset_content_rating = ['Everyone', 'Teen', 'Everyone 10+', 'Mature 17+', 'Adults only 18+', 'Unrated']
+	if not information_list[5] in dataset_content_rating:
+		ml_content_rating = 'Everyone'
+		print(information_list[5])
+	else:
+		ml_content_rating = information_list[5]
 
+	price = soup.find_all("button")
+	metas = price[5].find_all("meta")
+	meta = metas[-1].get("content")
+	if len(meta.split()) == 2:
+		price = float(meta.split()[1]) * 4.18
+	else:
+		price = 0
+	if price == 0:
+		ml_type = 'Free'
+	else:
+		ml_type = 'Paid'
+	
+	ml_price = str(price)
+	if 'GAME' in category.split('_'):
+		category = 'GAME'
+	ml_category = category
+	ml_android_ver = information_list[4]
+	ml_size = information_list[1]
+	ml_reviews = count
+	ml_last_updated = information_list[0]
+	ml_install = information_list[2]
+
+	test_data = pd.DataFrame([[ml_category, ml_content_rating, ml_android_ver, ml_type, ml_size, ml_install, ml_price, ml_reviews, ml_last_updated]], columns=['Category', 'Content Rating', 'Android Ver', 'Type', 'Size', 'Installs', 'Price', 'Reviews', 'Last Updated'])
+	print(test_data)
 	result = (rating, count, img, name)
+	
 	loaded_model = load('finalized_google_playstore.sav')
+	# predicted_rating = 0
+	predicted_rating = loaded_model.predict(test_data)
 
-
-	return render_template("improve.html", results = result, additional_informations = information_list)
+	return render_template("improve.html", results = result, additional_informations = information_list, prediction = predicted_rating)
--- testing.py
+++ testing.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, session, render_template, redirect, url_for, request
 
 
-res = requests.get("https://play.google.com/store/apps/details?id=com.google.android.youtube", headers={"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"})
+res = requests.get("https://play.google.com/store/apps/details?id=com.mojang.minecraftpe", headers={"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"})
 soup = BeautifulSoup(res.text, 'html.parser')
 # additional_information = soup.find_all("span", "htlgb")
 
@@ -33,13 +33,6 @@ soup = BeautifulSoup(res.text, 'html.parser')
 # 		print(a)
 
 # 	i = i + 1
-
-
-
-category = soup.find_all("a", "hrTbp R8zArc")[1]['href']
-category = category.split("/")[-1]
-
-print(category)
 
 
 # additional_informations = list(map(additional_informations.__getitem__, [0,1,2,3,4,5,7]))
@@ -85,4 +78,68 @@ print(category)
 
 
 
+additional_informations = soup.find_all("div", "hAyfc")
+additional_informations = list(map(additional_informations.__getitem__, [0,1,2,3,4,5,7]))
 
+information_list = list()
+# ['Updated', 'Size', 'Installs', 'Current Version', 'Requires Android', 'Content Rating', 'In-app Products']
+# print(additional_informations)
+
+# <h5>Updated: {{additional_informations[0]}}</h5>
+# <h5>Size: {{additional_informations[1]}}</h5>
+# <h5>Installs: {{additional_informations[2]}}</h5>
+# <h5>Current Version: {{additional_informations[3]}}</h5>
+# <h5>Requires Android: {{additional_informations[4]}}</h5>
+# <h5>Content Rating: {{additional_informations[5]}}</h5>
+# <h5>In-app Products: {{additional_informations[6]}}</h5>
+# <h5>Category: {{additional_informations[7]}}</h5>
+
+# print(additional_informations)
+
+if additional_informations[0].find("span", "htlgb").text == 'Learn More':
+	information_list.append(additional_informations[1].find("span", "htlgb").text)
+	information_list.append(additional_informations[2].find("span", "htlgb").text)
+	information_list.append(additional_informations[3].find("span", "htlgb").text)
+	information_list.append(additional_informations[4].find("span", "htlgb").text)
+	information_list.append(additional_informations[5].find("span", "htlgb").text)
+	additional_informations = soup.find_all("div", "hAyfc")
+	additional_informations = list(map(additional_informations.__getitem__, [6]))
+	information_list.append(additional_informations[0].find("span", "htlgb").text)
+	additional_informations = soup.find_all("div", "hAyfc")
+	additional_informations = list(map(additional_informations.__getitem__, [8]))
+	information_list.append((additional_informations[0].find("span", "htlgb").text).replace(u'\xa0', u' '))
+	
+	# information_list.append(additional_informations[7].find("span", "htlgb").text)
+
+# print(information_list)
+additional_informations = soup.find_all("div", "hAyfc")
+additional_informations = list(map(additional_informations.__getitem__, [8]))
+print(additional_informations)
+# i = 1
+# for additional_information in additional_informations:
+# 	if i == 6:
+# 		check_content = additional_information.find("div", "BgcNfc").text
+# 		if check_content != "Content Rating":
+# 			information_list.append("No content rating is available")
+# 		else:
+# 			content_rating = additional_information.find_all("div")[2].text
+# 			information_list.append(content_rating)
+# 	elif i == 7:
+# 		check_content = additional_information.find("div", "BgcNfc").text
+# 		if check_content != "In-app Products":
+# 			information_list.append("No in-app purchase is shown")
+# 		else:
+# 			in_app_purchase = additional_information.find("span", "htlgb").text
+# 			information_list.append(in_app_purchase)
+# 	else:
+# 		other_information = additional_information.find("span", "htlgb").text
+# 		information_list.append(other_information)
+# 	i = i + 1
+
+# https://play.google.com/store/apps/details?id=com.craftgames.worldcrft
+# ['November 13, 2019', '80M', '10,000,000+', '3.4.11', '4.1 and up', 'Rated for 3+', 'RM\xa03.84 - RM\xa0207.69 per item']
+# ['November 12, 2019', 'Varies with device', '10,000,000+', '1.13.1.5', '4.2 and up', 'Rated for 7+Fear, Mild ViolenceLearn More']
+# ['November 12, 2019', 'Varies with device', '10,000,000+', '1.13.1.5', '4.2 and up', 'Rated for 7+Fear, Mild ViolenceLearn More', 'RM 1.46 - RM 209.99 per item']
+
+
+print(information_list)
